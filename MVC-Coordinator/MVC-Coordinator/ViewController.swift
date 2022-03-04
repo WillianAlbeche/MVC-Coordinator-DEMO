00edec6ce6f61fdc0e1c804d2e9eecc44fb675bd
Willian Magnum Albeche <willianalbeche@gmail.com>
2022-03-04 14:31:31 -0300

    First commit + gitignore

---
+++ MVC-Coordinator/MVC-Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Coordinator
+//
+//  Created by Willian Magnum Albeche on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
